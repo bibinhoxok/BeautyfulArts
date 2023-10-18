b3301a2f57666c1da778d3fad7093cfdc1858647
ThuHang2604 <132872569+ThuHang2604@users.noreply.github.com>
2023-10-18 15:54:34 +0700

    add course manage, employee's header
    edit App.js

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,9 +1,6 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
-import Footer from "./components/Footer/footer";
+import Footer from './components/Footer/footer';
 import Header from './components/Header/header';
 import Home from './components/Home/baner';
 import Login from './components/Login/login';
@@ -11,24 +8,51 @@ import Register from './components/Register/register';
 import CourseList from './components/Course/courseList';
 import CourseDetail from './components/Course/courseDetail';
 import CreateCourse from './components/Course/createCourse';
+import CourseManagement from './components/Course/courseManagement';
+import EmployeeHeader from './components/Header/headerEmployee';
 
 function App() {
+  // Simulate user data with role information (1 for customer, 2 for instructor, 3 for staff, 4 for admin)
+  const user = { role: 2 }; // Change the role as needed
+
+  // Define a navigate function
+  const navigate = useNavigate();
+
+  // Define routes for different roles
+  const customerRoutes = (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/courseList" element={<CourseList />} />
+        <Route path="/courseDetail" element={<CourseDetail />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+
+  const employeeRoutes = (
+    <>
+      <EmployeeHeader />
+      <Routes>
+        <Route path="/courseManagement" element={<CourseManagement />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+
   return (
     <BrowserRouter>
-      <Header/>
       <Routes>
-      <Route path="/" element={<Home />} />
-      {/* <Route path="/" element={</>} /> */}
-      <Route path='/login' element={Login} />
-      <Route path='/register' element={Register} />
-      <Route path='/courseList' element={<CourseList/>} />
-      <Route path='/courseDetail' element={<CourseDetail/>} />
-      <Route path="/createCourse" element={<CreateCourse/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/createCourse" element={<CreateCourse />} />
+        {user.role === 1 && customerRoutes}
+        {user.role >= 2 && employeeRoutes}
+        {/* Add more roles as needed */}
       </Routes>
-      <Footer/>
     </BrowserRouter>
   );
-  
 }
 
 export default App;
